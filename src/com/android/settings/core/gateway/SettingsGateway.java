HashOS/android_packages_apps_Settings

[2/2] Settings: Add NetworkTraffic frag
Change-Id: Ia23376e242b2265e0b097f5d7a83974a5ebc600f

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -143,6 +143,7 @@ import org.hash.hashtweaks.ButtonBrightnessSettings;
 import org.hash.hashtweaks.ButtonCustomizationSettings;
 import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 import org.hash.hashtweaks.interfacesettings.BarsSettings;
+import org.hash.hashtweaks.interfacesettings.NetworkTrafficSettings;
 import org.hash.hashtweaks.interfacesettings.StatusbarBatterySettings;
 import org.hash.hashtweaks.interfacesettings.StatusbarClockSettings;
 import org.hash.hashtweaks.batterylight.BatteryLightSettings;
@@ -280,6 +281,7 @@ public class SettingsGateway {
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
             ButtonCustomizationSettings.class.getName(),
+            NetworkTrafficSettings.class.getName(),
             StatusbarClockSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -138,12 +138,14 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+<<<<<<< HEAD
 import org.echelon.echelontweaks.EchelonFragment;
 import org.echelon.echelontweaks.ButtonSettings;
 import org.echelon.echelontweaks.ButtonBrightnessSettings;
 import org.echelon.echelontweaks.ButtonCustomizationSettings;
 import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 import org.echelon.echelontweaks.interfacesettings.BarsSettings;
+import org.echelon.echelontweaks.interfacesettings.NetworkTrafficSettings;
 import org.echelon.echelontweaks.interfacesettings.StatusbarBatterySettings;
 import org.echelon.echelontweaks.interfacesettings.StatusbarClockSettings;
 import org.echelon.echelontweaks.batterylight.BatteryLightSettings;
@@ -282,6 +284,7 @@ public class SettingsGateway {
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
             ButtonCustomizationSettings.class.getName(),
+            NetworkTrafficSettings.class.getName(),
             StatusbarClockSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),