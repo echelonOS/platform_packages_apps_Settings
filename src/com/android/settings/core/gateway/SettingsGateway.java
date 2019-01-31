HashOS/android_packages_apps_Settings

[3/3] Settings: add weather parts
Change-Id: Ie67df05e8981f5fd64dd615b60ee0864c3f3dee8

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -143,6 +143,7 @@ import org.hash.hashtweaks.ButtonBrightnessSettings;
 import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 import org.hash.hashtweaks.interfacesettings.BarsSettings;
 import org.hash.hashtweaks.batterylight.BatteryLightSettings;
+import org.hash.hashtweaks.moresettings.OmniJawsSettings;
 
 public class SettingsGateway {
 
@@ -276,6 +277,7 @@ public class SettingsGateway {
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
             BatteryLightSettings.class.getName(),
+            OmniJawsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -144,6 +144,7 @@ import org.echelon.echelontweaks.ButtonBrightnessSettings;
 import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 import org.echelon.echelontweaks.interfacesettings.BarsSettings;
 import org.echelon.echelontweaks.batterylight.BatteryLightSettings;
+import org.echelon.echelontweaks.moresettings.OmniJawsSettings;
 
 
 public class SettingsGateway {
@@ -279,6 +280,7 @@ public class SettingsGateway {
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
             BatteryLightSettings.class.getName(),
+            OmniJawsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {