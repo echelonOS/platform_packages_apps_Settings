HashOS/android_packages_apps_Settings

[2/2] Settings: Add battery style frag
Change-Id: I9009370b14387d7643aa9fe9abbe732574bc6401

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -142,6 +142,7 @@ import org.hash.hashtweaks.ButtonSettings;
 import org.hash.hashtweaks.ButtonBrightnessSettings;
 import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 import org.hash.hashtweaks.interfacesettings.BarsSettings;
+import org.hash.hashtweaks.interfacesettings.StatusbarBatterySettings;
 import org.hash.hashtweaks.batterylight.BatteryLightSettings;
 import org.hash.hashtweaks.moresettings.OmniJawsSettings;
 import org.hash.hashtweaks.GestureSettings;
@@ -278,6 +279,7 @@ public class SettingsGateway {
             ButtonBrightnessSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
+            StatusbarBatterySettings.class.getName(),
             BatteryLightSettings.class.getName(),
             OmniJawsSettings.class.getName(),
             GestureSettings.class.getName(),

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -143,11 +143,11 @@ import org.echelon.echelontweaks.ButtonSettings;
 import org.echelon.echelontweaks.ButtonBrightnessSettings;
 import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 import org.echelon.echelontweaks.interfacesettings.BarsSettings;
+import org.echelon.echelontweaks.interfacesettings.StatusbarBatterySettings;
 import org.echelon.echelontweaks.batterylight.BatteryLightSettings;
 import org.echelon.echelontweaks.moresettings.OmniJawsSettings;
 import org.echelon.echelontweaks.GestureSettings;
 import org.echelon.echelontweaks.moresettings.MoreSettings;
-
 
 public class SettingsGateway {
 
@@ -281,6 +281,7 @@ public class SettingsGateway {
             ButtonBrightnessSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
+            StatusbarBatterySettings.class.getName(),
             BatteryLightSettings.class.getName(),
             OmniJawsSettings.class.getName(),
             GestureSettings.class.getName(),