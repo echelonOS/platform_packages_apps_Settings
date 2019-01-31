HashOS/android_packages_apps_Settings

[2/3] Settings: Ability to disable dashboards conditions
Change-Id: I687ac1a48120f9f2b0b42d5811b65fe71f5a1430

src/com/android/settings/dashboard/conditional/Condition.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.graphics.drawable.Drawable;
 import android.os.PersistableBundle;
+import android.provider.Settings;
 import android.support.annotation.VisibleForTesting;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -132,7 +133,9 @@ public abstract class Condition {
     }
 
     public boolean shouldShow() {
-        return isActive() && !isSilenced();
+        return isActive() && !isSilenced()
+               && ((Settings.System.getInt(mManager.getContext().getContentResolver(),
+               Settings.System.HASH_ENABLE_CONDITIONS, 1) == 1));
     }
 
     long getLastChange() {

echelonOS/platform_packages_apps_Settings

src/com/android/settings/dashboard/conditional/Condition.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.graphics.drawable.Drawable;
 import android.os.PersistableBundle;
+import android.provider.Settings;
 import android.support.annotation.VisibleForTesting;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -132,7 +133,9 @@ public abstract class Condition {
     }
 
     public boolean shouldShow() {
-        return isActive() && !isSilenced();
+        return isActive() && !isSilenced()
+               && ((Settings.System.getInt(mManager.getContext().getContentResolver(),
+               Settings.System.ECHELON_ENABLE_CONDITIONS, 1) == 1));
     }
 
     long getLastChange() {