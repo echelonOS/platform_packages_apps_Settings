HashOS/android_packages_apps_Settings

[2/2] Settings: Add Button Customization frag
Change-Id: Iad1b2ea26fbd9e463705d3ec79ce27ff5e5e2c21

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -140,6 +140,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import org.hash.hashtweaks.HashFragment;
 import org.hash.hashtweaks.ButtonSettings;
 import org.hash.hashtweaks.ButtonBrightnessSettings;
+import org.hash.hashtweaks.ButtonCustomizationSettings;
 import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 import org.hash.hashtweaks.interfacesettings.BarsSettings;
 import org.hash.hashtweaks.interfacesettings.StatusbarBatterySettings;
@@ -277,6 +278,7 @@ public class SettingsGateway {
             HashFragment.class.getName(),
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
+            ButtonCustomizationSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
             StatusbarBatterySettings.class.getName(),

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -141,6 +141,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import org.echelon.echelontweaks.EchelonFragment;
 import org.echelon.echelontweaks.ButtonSettings;
 import org.echelon.echelontweaks.ButtonBrightnessSettings;
+import org.echelon.echelontweaks.ButtonCustomizationSettings;
 import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 import org.echelon.echelontweaks.interfacesettings.BarsSettings;
 import org.echelon.echelontweaks.interfacesettings.StatusbarBatterySettings;
@@ -279,6 +280,7 @@ public class SettingsGateway {
             EchelonFragment.class.getName(),
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
+            ButtonCustomizationSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
             StatusbarBatterySettings.class.getName(),