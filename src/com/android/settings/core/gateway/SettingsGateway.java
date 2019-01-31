HashOS/android_packages_apps_Settings

Settings: add GestureSettings class name
Change-Id: If9b49007aca8f5b42396e2cc563dea987789d26d

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -144,6 +144,7 @@ import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 import org.hash.hashtweaks.interfacesettings.BarsSettings;
 import org.hash.hashtweaks.batterylight.BatteryLightSettings;
 import org.hash.hashtweaks.moresettings.OmniJawsSettings;
+import org.hash.hashtweaks.GestureSettings;
 
 public class SettingsGateway {
 
@@ -278,6 +279,7 @@ public class SettingsGateway {
             BarsSettings.class.getName(),
             BatteryLightSettings.class.getName(),
             OmniJawsSettings.class.getName(),
+            GestureSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -145,6 +145,7 @@ import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 import org.echelon.echelontweaks.interfacesettings.BarsSettings;
 import org.echelon.echelontweaks.batterylight.BatteryLightSettings;
 import org.echelon.echelontweaks.moresettings.OmniJawsSettings;
+import org.echelon.echelontweaks.GestureSettings;
 
 
 public class SettingsGateway {
@@ -281,6 +282,7 @@ public class SettingsGateway {
             BarsSettings.class.getName(),
             BatteryLightSettings.class.getName(),
             OmniJawsSettings.class.getName(),
+            GestureSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {