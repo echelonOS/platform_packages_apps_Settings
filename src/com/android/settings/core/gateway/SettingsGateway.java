HashOS/android_packages_apps_Settings

Settings: add BarsSettings class name
Change-Id: I460ba21cc53d9e63b0c40315b8035bf07064745b

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -141,6 +141,7 @@ import org.hash.hashtweaks.HashFragment;
 import org.hash.hashtweaks.ButtonSettings;
 import org.hash.hashtweaks.ButtonBrightnessSettings;
 import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
+import org.hash.hashtweaks.interfacesettings.BarsSettings;
 
 public class SettingsGateway {
 
@@ -272,6 +273,7 @@ public class SettingsGateway {
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
             LockscreenSettings.class.getName(),
+            BarsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -142,6 +142,7 @@ import org.echelon.echelontweaks.EchelonFragment;
 import org.echelon.echelontweaks.ButtonSettings;
 import org.echelon.echelontweaks.ButtonBrightnessSettings;
 import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
+import org.echelon.echelontweaks.interfacesettings.BarsSettings;
 
 public class SettingsGateway {
 
@@ -274,6 +275,7 @@ public class SettingsGateway {
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
             LockscreenSettings.class.getName(),
+            BarsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {