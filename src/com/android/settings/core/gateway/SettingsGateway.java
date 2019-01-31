HashOS/android_packages_apps_Settings

[2/3] Settings: Disable suggestions
Change-Id: Ifdc40f768d08eea46590e84b3f8baf8bd0f20aba

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -145,6 +145,7 @@ import org.hash.hashtweaks.interfacesettings.BarsSettings;
 import org.hash.hashtweaks.batterylight.BatteryLightSettings;
 import org.hash.hashtweaks.moresettings.OmniJawsSettings;
 import org.hash.hashtweaks.GestureSettings;
+import org.hash.hashtweaks.moresettings.MoreSettings;
 
 public class SettingsGateway {
 
@@ -280,6 +281,7 @@ public class SettingsGateway {
             BatteryLightSettings.class.getName(),
             OmniJawsSettings.class.getName(),
             GestureSettings.class.getName(),
+            MoreSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
@@ -21,6 +21,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.service.settings.suggestions.Suggestion;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.Pair;
@@ -57,7 +58,9 @@ public class SuggestionFeatureProviderImpl implements SuggestionFeatureProvider 
     public boolean isSuggestionEnabled(Context context) {
         final ActivityManager am =
                 (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        return !am.isLowRamDevice();
+        return !am.isLowRamDevice()
+                && (Settings.System.getInt(context.getContentResolver(),
+                Settings.System.HASH_ENABLE_SUGGESTIONS, 1) == 1);
     }
 
     @Override

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -146,6 +146,7 @@ import org.echelon.echelontweaks.interfacesettings.BarsSettings;
 import org.echelon.echelontweaks.batterylight.BatteryLightSettings;
 import org.echelon.echelontweaks.moresettings.OmniJawsSettings;
 import org.echelon.echelontweaks.GestureSettings;
+import org.echelon.echelontweaks.moresettings.MoreSettings;
 
 
 public class SettingsGateway {
@@ -283,6 +284,7 @@ public class SettingsGateway {
             BatteryLightSettings.class.getName(),
             OmniJawsSettings.class.getName(),
             GestureSettings.class.getName(),
+            MoreSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
@@ -21,6 +21,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.service.settings.suggestions.Suggestion;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.Pair;
@@ -57,7 +58,9 @@ public class SuggestionFeatureProviderImpl implements SuggestionFeatureProvider 
     public boolean isSuggestionEnabled(Context context) {
         final ActivityManager am =
                 (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        return !am.isLowRamDevice();
+        return !am.isLowRamDevice()
+                && (Settings.System.getInt(context.getContentResolver(),
+                Settings.System.ECHELON_ENABLE_SUGGESTIONS, 1) == 1);
     }
 
     @Override