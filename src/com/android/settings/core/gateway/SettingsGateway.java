HashOS/android_packages_apps_Settings

Settings: Add LockscreenSettings && ButtonBrightnessSettings class
Change-Id: Ia5296c8faa5380e27a5b45653c0dff6419ae6130

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -139,6 +139,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import org.hash.hashtweaks.HashFragment;
 import org.hash.hashtweaks.ButtonSettings;
+import org.hash.hashtweaks.ButtonBrightnessSettings;
+import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 
 public class SettingsGateway {
 
@@ -268,6 +270,8 @@ public class SettingsGateway {
             // hash additions start
             HashFragment.class.getName(),
             ButtonSettings.class.getName(),
+            ButtonBrightnessSettings.class.getName(),
+            LockscreenSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -140,6 +140,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import org.echelon.echelontweaks.EchelonFragment;
 import org.echelon.echelontweaks.ButtonSettings;
+import org.echelon.echelontweaks.ButtonBrightnessSettings;
+import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 
 public class SettingsGateway {
 
@@ -270,6 +272,8 @@ public class SettingsGateway {
             // echelon additions start
             EchelonFragment.class.getName(),
             ButtonSettings.class.getName(),
+            ButtonBrightnessSettings.class.getName(),
+            LockscreenSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {