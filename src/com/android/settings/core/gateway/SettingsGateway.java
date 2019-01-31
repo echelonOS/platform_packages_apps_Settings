HashOS/android_packages_apps_Settings

Settings: add BatteryLightSettings class name
Change-Id: I96acfb952f86b95932ddb063be0b6b9bc35fae4d

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -142,6 +142,7 @@ import org.hash.hashtweaks.ButtonSettings;
 import org.hash.hashtweaks.ButtonBrightnessSettings;
 import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 import org.hash.hashtweaks.interfacesettings.BarsSettings;
+import org.hash.hashtweaks.batterylight.BatteryLightSettings;
 
 public class SettingsGateway {
 
@@ -274,6 +275,7 @@ public class SettingsGateway {
             ButtonBrightnessSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
+            BatteryLightSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -143,6 +143,8 @@ import org.echelon.echelontweaks.ButtonSettings;
 import org.echelon.echelontweaks.ButtonBrightnessSettings;
 import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 import org.echelon.echelontweaks.interfacesettings.BarsSettings;
+import org.echelon.echelontweaks.batterylight.BatteryLightSettings;
+
 
 public class SettingsGateway {
 
@@ -276,6 +278,7 @@ public class SettingsGateway {
             ButtonBrightnessSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
+            BatteryLightSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {