HashOS/android_packages_apps_Settings

[2/2] Settings: Add Clock Customization frag
Change-Id: I5cd21dc52f1709983b638c6925352536c5f0e13c

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -144,6 +144,7 @@ import org.hash.hashtweaks.ButtonCustomizationSettings;
 import org.hash.hashtweaks.interfacesettings.LockscreenSettings;
 import org.hash.hashtweaks.interfacesettings.BarsSettings;
 import org.hash.hashtweaks.interfacesettings.StatusbarBatterySettings;
+import org.hash.hashtweaks.interfacesettings.StatusbarClockSettings;
 import org.hash.hashtweaks.batterylight.BatteryLightSettings;
 import org.hash.hashtweaks.moresettings.OmniJawsSettings;
 import org.hash.hashtweaks.GestureSettings;
@@ -279,6 +280,7 @@ public class SettingsGateway {
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
             ButtonCustomizationSettings.class.getName(),
+            StatusbarClockSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
             StatusbarBatterySettings.class.getName(),

echelonOS/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -145,6 +145,7 @@ import org.echelon.echelontweaks.ButtonCustomizationSettings;
 import org.echelon.echelontweaks.interfacesettings.LockscreenSettings;
 import org.echelon.echelontweaks.interfacesettings.BarsSettings;
 import org.echelon.echelontweaks.interfacesettings.StatusbarBatterySettings;
+import org.echelon.echelontweaks.interfacesettings.StatusbarClockSettings;
 import org.echelon.echelontweaks.batterylight.BatteryLightSettings;
 import org.echelon.echelontweaks.moresettings.OmniJawsSettings;
 import org.echelon.echelontweaks.GestureSettings;
@@ -281,6 +282,7 @@ public class SettingsGateway {
             ButtonSettings.class.getName(),
             ButtonBrightnessSettings.class.getName(),
             ButtonCustomizationSettings.class.getName(),
+            StatusbarClockSettings.class.getName(),
             LockscreenSettings.class.getName(),
             BarsSettings.class.getName(),
             StatusbarBatterySettings.class.getName(),